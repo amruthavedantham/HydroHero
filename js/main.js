949e825f89f178e64669867054ea1150e1085ca4
Anushree Anil <anushreeanil@Anushrees-MacBook-Air.local>
2026-03-06 05:57:07 +0530

    Updated pages and styling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 const navToggle = document.getElementById("navToggle");
 const navMenu = document.getElementById("navMenu");
 
+if(navToggle){
 navToggle.addEventListener("click", () => {
 navMenu.classList.toggle("show");
 });
+}
+
+let user = JSON.parse(localStorage.getItem("hydroUser"));
+
+let currentPage = window.location.pathname.split("/").pop();
+
+if(!user && currentPage !== "login.html" && currentPage !== "signup.html"){
+window.location.href = "login.html";
+}
+
+function logout(){
+
+localStorage.removeItem("hydroUser");
+
+window.location.href = "login.html";
+
+}
